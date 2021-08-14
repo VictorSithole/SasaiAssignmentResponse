9503912c9daf7327fd801e2a8a2fe1ba9785abc5
VictorSithole <50654408+VictorSithole@users.noreply.github.com>
2021-08-14 21:57:01 +0200

    Update EpayResource.java

--- electronic-payments-api/src/main/java/com/econetwireless/epay/api/rest/resources/EpayResource.java
+++ electronic-payments-api/src/main/java/com/econetwireless/epay/api/rest/resources/EpayResource.java
@@ -6,7 +6,6 @@ import com.econetwireless.epay.api.rest.messages.TransactionsResponse;
 import com.econetwireless.utils.messages.AirtimeBalanceResponse;
 import com.econetwireless.utils.messages.AirtimeTopupRequest;
 import com.econetwireless.utils.messages.AirtimeTopupResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,12 +18,16 @@ public class EpayResource {
 
     private EpayRequestProcessor epayRequestProcessor;
 
-
     private ReportingProcessor reportingProcessor;
+
+    public EpayResource(EpayRequestProcessor epayRequestProcessor, ReportingProcessor reportingProcessor) {
+        this.epayRequestProcessor = epayRequestProcessor;
+        this.reportingProcessor = reportingProcessor;
+    }
 
     @GetMapping(value = "enquiries/{partnerCode}/balances/{mobileNumber}",
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public AirtimeBalanceResponse enquireAirtimeBalance( final String pCode, @PathVariable("mobileNumber") final String msisdn) {
+    public AirtimeBalanceResponse enquireAirtimeBalance(@PathVariable("partnerCode") final String pCode, @PathVariable("mobileNumber") final String msisdn) {
         return epayRequestProcessor.enquireAirtimeBalance(pCode, msisdn);
     }
 
