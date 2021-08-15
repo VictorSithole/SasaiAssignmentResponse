16f386b4cb34e933abb946e97d3201ec72dcb4ee
VictorSithole <50654408+VictorSithole@users.noreply.github.com>
2021-08-15 21:17:20 +0200

    added intillegent networkservice binder class to the module electronic-payments-business

--- electronic-payments-business/src/main/java/com/econetwireless/epay/business/services/impl/ReportingServiceImpl.java
+++ electronic-payments-business/src/main/java/com/econetwireless/epay/business/services/impl/ReportingServiceImpl.java
@@ -3,6 +3,9 @@ package com.econetwireless.epay.business.services.impl;
 import com.econetwireless.epay.business.services.api.ReportingService;
 import com.econetwireless.epay.dao.subscriberrequest.api.SubscriberRequestDao;
 import com.econetwireless.epay.domain.SubscriberRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -10,6 +13,7 @@ import java.util.List;
  * Created by tnyamakura on 18/3/2017.
  */
 public class ReportingServiceImpl implements ReportingService {
+    Logger logger = LoggerFactory.getLogger(ReportingServiceImpl.class);
 
     private SubscriberRequestDao subscriberRequestDao;
 
--- electronic-payments-business/target/generated-sources/com/econetwireless/in/webservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://service.soap.in.econetwireless.com/")
-package com.econetwireless.in.webservice;
